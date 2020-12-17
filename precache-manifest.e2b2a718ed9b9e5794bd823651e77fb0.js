46cd67c44b5e99d91e17065cb8323855d7dd1315
Барт Даниил <52742955+pelemen228@users.noreply.github.com>
2020-12-17 23:44:47 +0600

    Add files via upload

---
+++ precache-manifest.e2b2a718ed9b9e5794bd823651e77fb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0b592714deedf44068213513c16e9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04af214a92fad8764b8a",
+    "url": "/static/css/2.ae79daaa.chunk.css"
+  },
+  {
+    "revision": "cdbd2cd1be22345af7f2",
+    "url": "/static/css/main.08abd6a5.chunk.css"
+  },
+  {
+    "revision": "04af214a92fad8764b8a",
+    "url": "/static/js/2.5536c126.chunk.js"
+  },
+  {
+    "revision": "dc08264783d19f0ba1d90e984952f964",
+    "url": "/static/js/2.5536c126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbd2cd1be22345af7f2",
+    "url": "/static/js/main.d5b92cd7.chunk.js"
+  },
+  {
+    "revision": "b32055399593b88aa579",
+    "url": "/static/js/runtime-main.bbfc2ff9.js"
+  }
+]);
