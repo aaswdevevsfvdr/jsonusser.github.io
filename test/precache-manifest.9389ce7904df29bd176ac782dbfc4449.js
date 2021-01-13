4cc3640cc560e99f6cedebfa51553d776d6298a7
Барт Даниил <52742955+pelemen228@users.noreply.github.com>
2021-01-13 21:04:43 +0600

    Add files via upload

---
+++ test/precache-manifest.9389ce7904df29bd176ac782dbfc4449.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c46e6138505cd2cdc6b9",
+    "url": "/static/js/main.5aa34611.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "57da264036c727763e45",
+    "url": "/static/css/2.2d6169ee.chunk.css"
+  },
+  {
+    "revision": "57da264036c727763e45",
+    "url": "/static/js/2.e429280c.chunk.js"
+  },
+  {
+    "revision": "776000ce872d21f0f1617e7954f3466f",
+    "url": "/index.html"
+  }
+];
