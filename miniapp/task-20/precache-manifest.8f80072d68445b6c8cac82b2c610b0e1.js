6d42ec16dfb8b7644b854243f50e94d8264ccc85
Барт Даниил <52742955+jsonusser@users.noreply.github.com>
2021-05-22 22:50:58 +0600

    Add files via upload

---
+++ miniapp/task-20/precache-manifest.8f80072d68445b6c8cac82b2c610b0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f330fadca52d32ae16c34e8943b628",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71ec826e92155ee292b5",
+    "url": "./static/css/2.b0147297.chunk.css"
+  },
+  {
+    "revision": "71ec826e92155ee292b5",
+    "url": "./static/js/2.97cdb1ba.chunk.js"
+  },
+  {
+    "revision": "47cfb1ea3feeeddc4907d1f096ee6bd4",
+    "url": "./static/js/2.97cdb1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bcf98bedc7d9b8fcb8e",
+    "url": "./static/js/main.1bb5004b.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
