825f1c14e195680cb9b9363836cedfb07fd38f39
Барт Даниил <52742955+jsonusser@users.noreply.github.com>
2021-05-22 23:41:20 +0600

    Add files via upload

---
+++ miniapp/task-40/precache-manifest.ed9f2f0ba20b34dabe1200d7d10a0440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4808592ef38bdf8034e1803eab51c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54a2eb06f13a1c9de45a",
+    "url": "./static/css/2.b0147297.chunk.css"
+  },
+  {
+    "revision": "2e3c1b49a5873db48c3c",
+    "url": "./static/css/main.904e36c2.chunk.css"
+  },
+  {
+    "revision": "54a2eb06f13a1c9de45a",
+    "url": "./static/js/2.17d07e19.chunk.js"
+  },
+  {
+    "revision": "47cfb1ea3feeeddc4907d1f096ee6bd4",
+    "url": "./static/js/2.17d07e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3c1b49a5873db48c3c",
+    "url": "./static/js/main.b6e0642b.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
